8852ee381faa08802ea3061c505d78159bad8bfc
Carson Clark <carsonclark@mbp14.local>
2024-02-04 11:01:02 -0800

    refresh on state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,15 @@
 // className because class is a reserved keyword in JSX/JS
-import { MouseEvent } from "react";
+// import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["Saint John", "Victoria", "Toronto", "Winnipeg"];
-  //   items = [];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  // type annotation
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  };
+  // event handler
+  // const handleClick = (event: MouseEvent) => {
+  //   console.log(event);
+  // };
 
   return (
     <>
@@ -17,8 +18,14 @@ function ListGroup() {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item"
-            onClick={(event) => console.log(item, index, event)}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
             key={item}
           >
             {item}
